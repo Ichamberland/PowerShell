537f58fb44e6216355bc597fedb43c9ec23f47ad
Ichamberland <ian.a.chamberland@gmail.com>
2016-09-20 11:42:18 -0600

    Add files via upload

---
+++ 01-InstanceSetup/02 - Jobs.ps1
@@ -0,0 +1,23 @@
+﻿import-module sqlps | out-null
+clear
+
+#When SQL Logins are scripted, they are by default disabled and have a random password.
+#This is less then ideal as there are better ways to transfer the logins. 
+
+$SourceInstance = 'van-sql2008v1\sql01'
+$DestInstance   = 'van-sql2014v1\sql01'
+
+$Jobs = Get-ChildItem "SQLSERVER:\\SQL\\$SourceInstance\\JobServer\\Jobs\\" | where {$_.Name -inotmatch '##' -and $_.Name -inotmatch 'sa' -and $_.Name -inotmatch 'NT '}
+
+$SQL = ''
+
+FOREACH ($Job in $Jobs)
+{
+    $SQL += $Job.Script()
+}
+
+$SQL
+
+
+#Invoke-Sqlcmd -ServerInstance $DestInstance -Query $SQL
+
---
+++ 02-Mirroring/01-SetupMirroring.ps1
@@ -0,0 +1,30 @@
+﻿#Powershell module providing supporting functions
+Import-Module '\\van-nas01\Backups\Scripts\Powershell\Demo\PS_SQL_Functions.psm1'
+set-location "c:"
+
+# Script Configuration 
+$PrincipalInstance ='VAN-SQL2008v1\sql01' 
+$SecondaryInstance = 'VAN-SQL2014v1\sql01'
+$PrincipalEndpoint ='TCP://VAN-SQL2008v1.Iac.Net:5022' 
+$MirrorEndpoint ='TCP://VAN-SQL2014v1.iac.net:5022'
+
+# Generate Database List
+# This can either be generated from SQL Server or Controlled by File
+$Databases = Get-ChildItem "SQLServer:\SQL\$PrincipalInstance\Databases\" | where {$_.Name -ne 'DBMaint'}
+#$Databases = Get-Content .\DBList.txt
+
+
+#Loop through databases and configure mirroring based on configuration defined above
+Foreach ($database in $Databases)
+{
+    write-host "Restoring Database: $database" -BackgroundColor Red
+
+    Restore-DatabaseBackup -SourceInstance  $PrincipalInstance  -SourceDatabase $database.Name -DestInstance $SecondaryInstance -DestDatabase $database.Name -NoRecovery
+
+    $SQLMirror  = "ALTER DATABASE $($Database.Name) SET PARTNER = N'$PrincipalEndpoint';"
+    $SQLPrimary = "ALTER DATABASE $($Database.Name) SET PARTNER = N'$MirrorEndpoint'; ALTER DATABASE $($Database.Name) Set Safety Off;"
+
+    Invoke-Sqlcmd -ServerInstance $SecondaryInstance -Query $SQLMirror
+    Invoke-Sqlcmd -ServerInstance $PrincipalInstance -Query $SQLPrimary
+
+}
